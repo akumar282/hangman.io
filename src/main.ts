991127891d4f833a45bd57d95d1f1b9f78c6a459
akumar282 <abhi.kumar7567@gmail.com>
2022-09-09 00:56:57 -0700

    server?

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log('hello')
+import { createServer } from 'http'
+import { Server, Socket } from 'socket.io'
+
+const httpServer = createServer()
+const io = new Server(httpServer, {
+  // ...
+})
+
+io.on('connection', (socket: Socket) => {
+  // ...
+})
+
+httpServer.listen(3000)
