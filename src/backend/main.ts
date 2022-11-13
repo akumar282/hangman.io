04b56194aba4b4f0ec920e5123f0dcabffadf5c6
akumar282 <abhi.kumar7567@gmail.com>
2022-11-13 01:56:22 -0800

    new structure

---
+++ src/backend/main.ts
@@ -0,0 +1,15 @@
+type GenreWordPoints = {
+  genre: string
+  keyword: string
+  points: number
+  book?: string
+  firstPVal?: number
+}
+
+const tteestrunsh : GenreWordPoints = {
+  genre: 'test',
+  keyword: 'done',
+  points: 1,
+  book: 'erg',
+  firstPVal: 235423
+}
