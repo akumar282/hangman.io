93601b3c248fe6a9d4983d0d9ec1878e40d3c268
mohazahid <adyanzahid786@gmail.com>
2023-01-06 00:48:59 -0800

    start of create page

--- src/pages/create_game.js
+++ src/pages/create_game.js
@@ -1,8 +1,30 @@
+import React from 'react';
+import './css/createStyles.scss';
+
+const tagsList = ['Easy', 'Medium', 'Hard', 'Programming', 'Movies', 'Music', 'Sports', 'Animals', 'Food', 'History', 'Geography', 'Science', 'Art', 'Literature', 'Math', 'Miscellaneous'];
 export default function CreateGame () {
     return (
       <div className = "Game">
-        <h1> About</h1>
+        <div className = "CreateTitle">
+          <h1> Create a Game </h1>
+        </div>
+          <h2> Choose Tags:</h2>
+        <div className = "containCar">
+             <div className = "CreateTagsCarosel"> 
+            {tagsList.map((tag) => 
+                <CustomTags props = {tag}/>
+            )}
+          </div>
+        </div>
+        <div className = "CreateGameForm">
+        </div>
       </div>
     )
   }
-  
+  function CustomTags (props) {
+    return (
+      <button className = "CustomTags">
+        <h3> {props.props} </h3>
+      </button>
+    )
+  }
